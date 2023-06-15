0804cbf03c297d8efccd40515196052ec2c1148f
Jose Daniel Romero <danyrome12@gmail.com>
2023-06-15 14:06:14 -0500

    Se optimizaron lineas de codigo

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -3,17 +3,28 @@ import '../stylesheets/UserForm.css'
 function UserForm() {
     return (
         <form id='user-form'>
-
             <label className='form-labels'>
               CARDHOLDER NAME
-              <input type='text' className='form-inputs large' name='input-name' placeholder='e.g. Jane Appleseed' spellCheck='false'/>
+              <input
+                type='text'
+                className='form-inputs large'
+                name='input-name'
+                placeholder='e.g. Jane Appleseed'
+                spellCheck='false'
+                autoComplete='off'
+              />
             </label>
-
             <label className='form-labels'>
               CARDHOLDER NUMBER
-              <input type='number' className='form-inputs large' name='input-number' placeholder='e.g. 1234 5678 9123 0000' spellCheck='false'/>
+              <input
+                type='number'
+                className='form-inputs large' 
+                name='input-number' 
+                placeholder='e.g. 1234 5678 9123 0000' 
+                spellCheck='false' 
+                autoComplete='off'
+              />
             </label>
-
             <section className='user-form-adjust'>
                 <div className='user-form-row'>
                   <label className='form-labels inline'>
@@ -24,14 +35,33 @@ function UserForm() {
                   </label>
                 </div>
                 <div className='user-form-row buttons'>
-                    <input type='number' className='form-inputs exp-style' name='input-expiration' placeholder='MM' spellCheck='false'/>
-                    <input type='number' className='form-inputs exp-style' name='input-expiration' placeholder='YY' spellCheck='false'/>
-                    <input type='number' className='form-inputs large adjust' name='input-cvc' placeholder='e.g. 123' spellCheck='false'/>
+                    <input
+                      type='number' 
+                      className='form-inputs exp-style' 
+                      name='input-expiration' 
+                      placeholder='MM' 
+                      spellCheck='false' 
+                      autoComplete='off'
+                    />
+                    <input
+                      type='number' 
+                      className='form-inputs exp-style' 
+                      name='input-expiration' 
+                      placeholder='YY' 
+                      spellCheck='false' 
+                      autoComplete='off'
+                    />
+                    <input 
+                      type='number' 
+                      className='form-inputs large adjust' 
+                      name='input-cvc' 
+                      placeholder='e.g. 123' 
+                      spellCheck='false'
+                      autoComplete='off'
+                    />
                 </div>
             </section>
-
             <button type='submit' className='button-submit'>Confirm</button>
-
         </form>
     );
 }
